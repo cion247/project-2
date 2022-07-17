7c5cf42c3645a41d4315613fc94793a382acc54c
cion247 <96902253+cion247@users.noreply.github.com>
2022-07-17 21:56:42 +0530

    three

--- components/Header.js
+++ components/Header.js
@@ -5,19 +5,19 @@ const Header = () => {
     return (
         <header class="text-gray-400 bg-transparent body-font">
             <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <Image src="/pages/earth.png"
-                    alt='log0'
-                    width={220}
-                    height={220} />
-                <a class="flex title-font font-medium items-center text-black mb-4 md:mb-0">
+
+                <a class="flex title-font font-medium items-center text-black mb-4 md:mb-0 " href="../pages/index.tsx">
+
+                    <img src="public/earth.png" alt="..." class="shadow-lg rounded max-w-full h-auto align-middle border-none" />
+
                     <span class="ml-3 text-xl">MY WORLD</span>
 
                 </a>
                 <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    <a class="mr-5 hover:text-white">First Link</a>
-                    <a class="mr-5 hover:text-white">Second Link</a>
-                    <a class="mr-5 hover:text-white">Third Link</a>
-                    <a class="mr-5 hover:text-white">Fourth Link</a>
+                    <a class="mr-5 hover:text-white">Room 1</a>
+                    <a class="mr-5 hover:text-white">Room 2</a>
+                    <a class="mr-5 hover:text-white">Room 3</a>
+                    <a class="mr-5 hover:text-white">Room 4</a>
                 </nav>
 
             </div>
