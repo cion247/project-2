5f1cedd947fd0bfd7f87d3d5f492a21113d07792
cion247 <96902253+cion247@users.noreply.github.com>
2022-07-25 21:28:55 +0530

    YESSS

--- components/BgOne.js
+++ components/BgOne.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from "react"
-import Canvas from '../components/canvas'
+//import Canvas from './canvas2'
+import Script from 'next/script'
 
 
 const BgOne = () => {
@@ -30,12 +31,136 @@ const BgOne = () => {
 
     const grRef = useRef();
 
+    useEffect(() => {
+        console.log("use effect activated");
+        // Initial Setup
+        var canvas = document.querySelector('canvas');
+        var c = canvas.getContext('2d');
+
+        canvas.width = innerWidth;
+        canvas.height = innerHeight;
+
+
+        // Variables
+        var mouse = {
+            x: innerWidth / 2,
+            y: innerHeight / 2
+        };
+
+        var colors = [
+            '#2185C5',
+            '#7ECEFD',
+            '#FFF6E5',
+            '#FF7F66'
+        ];
+
+        var gravity = 0.2;
+        var friction = 0.98;
+
+
+        // Event Listeners
+        addEventListener("mousemove", function (event) {
+            mouse.x = event.clientX;
+            mouse.y = event.clientY;
+        });
+
+        addEventListener("resize", function () {
+            canvas.width = innerWidth;
+            canvas.height = innerHeight;
+            init();
+        });
+
+        addEventListener("click", function (event) {
+            init();
+            console.log("start");
+        });
+
+
+        // Utility Functions
+        function randomIntFromRange(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+
+        function randomColor(colors) {
+            return colors[Math.floor(Math.random() * colors.length)];
+        }
+
+
+        // Objects
+        function Ball(x, y, dx, dy, radius, color) {
+            this.x = x;
+            this.y = y;
+            this.dx = dx;
+            this.dy = dy;
+            this.radius = radius;
+            this.color = color;
+
+            this.update = function () {
+                if (this.y + this.radius + this.dy > canvas.height) {
+                    this.dy = -this.dy;
+                    this.dy = this.dy * friction;
+                    this.dx = this.dx * friction;
+                } else {
+                    this.dy += gravity;
+                }
+
+                if (this.x + this.radius >= canvas.width || this.x - this.radius <= 0) {
+                    this.dx = -this.dx * friction;
+                }
+
+                this.x += this.dx;
+                this.y += this.dy;
+                this.draw();
+            };
+
+            this.draw = function () {
+                c.beginPath();
+                c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+                c.fillStyle = this.color;
+                c.fill();
+                c.stroke();
+                c.closePath();
+            };
+        }
+
+
+        // Implementation
+        var ballArray = [];
+
+        function init() {
+            ballArray = [];
+
+            for (let i = 0; i < 600; i++) {
+                var radius = randomIntFromRange(8, 20);
+                var x = randomIntFromRange(radius, canvas.width - radius);
+                var y = randomIntFromRange(0, canvas.height - radius);
+                var dx = randomIntFromRange(-3, 3)
+                var dy = randomIntFromRange(-2, 2)
+                ballArray.push(new Ball(x, y, dx, dy, radius, randomColor(colors)));
+            }
+        }
+
+        // Animation Loop
+        function animate() {
+            requestAnimationFrame(animate);
+
+            c.clearRect(0, 0, canvas.width, canvas.height);
+
+            for (let i = 0; i < ballArray.length; i++) {
+                ballArray[i].update();
+            }
+        }
+
+        init();
+        animate();
+    });
+
 
 
 
     return (
-        <section class="text-white text-shadow-xl body-font  justify-center ">
-            <div class="container  mx-auto flex px-5 py-5 flex-row itmes-start">
+        <section className="text-white text-shadow-xl body-font  justify-center ">
+            <div className="container  mx-auto flex px-5 py-5 flex-row itmes-start">
                 <div>
                     <button onClick={() => grRef.current.callFunction(1)} className=" mb-4 h-7 items-center  bg-blue-500 border-0  px-2  hover:bg-blue-600 rounded">start</button>
                     <p>gravity</p>
@@ -58,9 +183,10 @@ const BgOne = () => {
                 </div>
 
 
-                <Canvas ref={grRef} className="w-11/12 h-11/12 justify-center bg-white" />
-                {/* <canvas className="w-11/12 h-11/12 justify-center bg-white"></canvas>
-                <script src="/canvas2.js" /> */}
+                {/* <Canvas ref={grRef} className="w-11/12 h-11/12 justify-center bg-white" /> */}
+                {/* <canvas className="w-11/12 h-11/12 justify-center "></canvas> */}
+                <canvas className="w-11/12 h-11/12 justify-center bg-white"></canvas>
+
 
 
 
--- components/Header.js
+++ components/Header.js
@@ -4,31 +4,31 @@ import Link from 'next/link'
 
 const Header = () => {
     return (
-        <header class="text-white body-font text-shadow-xl ">
-            <div class="container  mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <header className="text-white body-font text-shadow-xl ">
+            <div className="container  mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
 
-                <Link href="/"><a class="flex title-font font-medium items-center  mb-4 md:mb-0 " href="/">
+                <Link href="/"><a className="flex title-font font-medium items-center  mb-4 md:mb-0 " href="/">
 
-                    <Image src="/earth.png" alt="..." width='50' height='40' objectFit='contain' class="ml-3 rounded align-center" />
+                    <Image src="/earth.png" alt="..." width='50' height='40' objectFit='contain' className="ml-3 rounded align-center" />
 
 
-                    <span class="ml-3 text-3xl">MY WORLD</span>
+                    <span className="ml-3 text-3xl">MY WORLD</span>
                 </a></Link>
 
-                <nav class="md:ml-auto  flex flex-wrap items-center justify-center  text-lg ">
+                <nav className="md:ml-auto  flex flex-wrap items-center justify-center  text-lg ">
 
-                    <Link href="/room1"><a target="_blank" rel="noopener noreferrer" class=" mx-3 px-3 my-1 pb-1 bg-green-900 rounded 
+                    <Link href="/room1"><a target="_blank" rel="noopener noreferrer" className=" mx-3 px-3 my-1 pb-1 bg-green-900 rounded 
                         hover:bg-green-700 ">Room 1
                     </a></Link>
 
-                    <Link href="/room2"><a target="_blank" rel="noopener noreferrer" class=" mx-3 px-3 my-1 pb-1 bg-green-900 rounded 
+                    <Link href="/room2"><a target="_blank" rel="noopener noreferrer" className=" mx-3 px-3 my-1 pb-1 bg-green-900 rounded 
                         hover:bg-green-700 ">Room 2
                     </a></Link>
 
-                    <Link href="/room3"><a target="_blank" rel="noopener noreferrer" class=" mx-3 px-3 my-1 pb-1 rounded bg-green-900  hover:bg-green-700">Room 3
+                    <Link href="/room3"><a target="_blank" rel="noopener noreferrer" className=" mx-3 px-3 my-1 pb-1 rounded bg-green-900  hover:bg-green-700">Room 3
                     </a></Link>
 
-                    <Link href="/room4"><a target="_blank" rel="noopener noreferrer" class=" mx-3 px-3 my-1 pb-1 rounded bg-green-900  hover:bg-green-700">Room 4
+                    <Link href="/room4"><a target="_blank" rel="noopener noreferrer" className=" mx-3 px-3 my-1 pb-1 rounded bg-green-900  hover:bg-green-700">Room 4
                     </a></Link>
 
                 </nav>
