84624ff11d848c309670b49f03b5cfe6f816a5b9
cion247 <96902253+cion247@users.noreply.github.com>
2022-07-27 01:04:37 +0530

    heart

--- components/BgOne.js
+++ components/BgOne.js
@@ -1,202 +1,127 @@
-import React, { useRef, useEffect, useState } from "react"
-//import Canvas from './canvas2'
-import Script from 'next/script'
-
+import React, { useEffect, useState } from "react"
 
 const BgOne = () => {
 
-    const [gravity, setGravity] = useState(1)
-    const [friction, setFriction] = useState(1)
-    const [radius, setRadius] = useState(20)
+    const [gravity, setGravity] = useState(0.2)
+    const [friction, setFriction] = useState(0.98)
+    const [balls, setBalls] = useState(150)
+    var colors = ['#62c0ff', '#41abf2', '#ffc7f7', '#939aff']
 
-    function decrementGravity() {
-        setGravity(prevGravity => prevGravity - 1)
-        { () => grRef.current.callFunction(1) }
-    }
-    function incrementGravity() {
-        setGravity(prevGravity => prevGravity + 1)
-    }
-    function decrementFriction() {
-        setFriction(prevFriction => prevFriction - 1)
-    }
-    function incrementFriction() {
-        setFriction(prevFriction => prevFriction + 1)
-    }
-    function decrementRadius() {
-        setRadius(prevRadius => prevRadius - 10)
-    }
-    function incrementRadius() {
-        setRadius(prevRadius => prevRadius + 10)
-    }
-
-    const grRef = useRef();
+    const decrementGravity = () => setGravity(prevGravity => prevGravity - 1)
+    const incrementGravity = () => setGravity(prevGravity => prevGravity + 1)
+    const decrementFriction = () => setFriction(prevFriction => prevFriction + 0.1)
+    const incrementFriction = () => setFriction(prevFriction => prevFriction - 0.1)
+    const decrementballs = () => setBalls(prevBalls => prevBalls - 50)
+    const incrementBalls = () => setBalls(prevBalls => prevBalls + 50)
+    const randomIntFromRange = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+    const randomColor = (colors) => colors[Math.floor(Math.random() * colors.length)]
 
     useEffect(() => {
-        console.log("use effect activated");
-        // Initial Setup
-        var canvas = document.querySelector('canvas');
-        var c = canvas.getContext('2d');
+        console.log("use effect activated")
 
-        canvas.width = innerWidth;
-        canvas.height = innerHeight;
+        var canvas = document.querySelector('canvas')
+        var c = canvas.getContext('2d')
+        canvas.width = innerWidth
+        canvas.height = innerHeight
 
+        addEventListener("click", () => init())
 
-        // Variables
-        var mouse = {
-            x: innerWidth / 2,
-            y: innerHeight / 2
-        };
-
-        var colors = [
-            '#2185C5',
-            '#7ECEFD',
-            '#FFF6E5',
-            '#FF7F66'
-        ];
-
-        var gravity = 0.2;
-        var friction = 0.98;
-
-
-        // Event Listeners
-        addEventListener("mousemove", function (event) {
-            mouse.x = event.clientX;
-            mouse.y = event.clientY;
-        });
-
-        addEventListener("resize", function () {
-            canvas.width = innerWidth;
-            canvas.height = innerHeight;
-            init();
-        });
-
-        addEventListener("click", function (event) {
-            init();
-            console.log("start");
-        });
-
-
-        // Utility Functions
-        function randomIntFromRange(min, max) {
-            return Math.floor(Math.random() * (max - min + 1) + min);
-        }
-
-        function randomColor(colors) {
-            return colors[Math.floor(Math.random() * colors.length)];
-        }
-
-
-        // Objects
         function Ball(x, y, dx, dy, radius, color) {
-            this.x = x;
-            this.y = y;
-            this.dx = dx;
-            this.dy = dy;
-            this.radius = radius;
-            this.color = color;
+            this.x = x; this.y = y; this.dx = dx; this.dy = dy; this.radius = radius; this.color = color
 
-            this.update = function () {
+            this.update = () => {
                 if (this.y + this.radius + this.dy > canvas.height) {
-                    this.dy = -this.dy;
-                    this.dy = this.dy * friction;
-                    this.dx = this.dx * friction;
+                    this.dy = -this.dy
+                    this.dy = this.dy * friction
+                    this.dx = this.dx * friction
                 } else {
-                    this.dy += gravity;
+                    this.dy += gravity
                 }
 
                 if (this.x + this.radius >= canvas.width || this.x - this.radius <= 0) {
-                    this.dx = -this.dx * friction;
+                    this.dx = -this.dx * friction
                 }
 
-                this.x += this.dx;
-                this.y += this.dy;
-                this.draw();
-            };
+                this.x += this.dx
+                this.y += this.dy
+                this.draw()
+            }
 
-            this.draw = function () {
-                c.beginPath();
-                c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-                c.fillStyle = this.color;
-                c.fill();
-                c.stroke();
-                c.closePath();
-            };
+            this.draw = () => {
+                c.beginPath()
+                c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+                c.fillStyle = this.color
+                c.fill()
+                c.stroke()
+                c.closePath()
+            }
         }
+        var ballArray = []
+        const init = () => {
+            ballArray = []
 
-
-        // Implementation
-        var ballArray = [];
-
-        function init() {
-            ballArray = [];
-
-            for (let i = 0; i < 600; i++) {
-                var radius = randomIntFromRange(8, 20);
-                var x = randomIntFromRange(radius, canvas.width - radius);
-                var y = randomIntFromRange(0, canvas.height - radius);
+            for (let i = 0; i < balls; i++) {
+                var radius = randomIntFromRange(10, 30)
+                var x = randomIntFromRange(radius, canvas.width - radius)
+                var y = randomIntFromRange(0, canvas.height - radius)
                 var dx = randomIntFromRange(-3, 3)
                 var dy = randomIntFromRange(-2, 2)
-                ballArray.push(new Ball(x, y, dx, dy, radius, randomColor(colors)));
+                ballArray.push(new Ball(x, y, dx, dy, radius, randomColor(colors)))
             }
         }
 
-        // Animation Loop
-        function animate() {
+        const animate = () => {
             requestAnimationFrame(animate);
-
-            c.clearRect(0, 0, canvas.width, canvas.height);
+            c.clearRect(0, 0, canvas.width, canvas.height)
 
             for (let i = 0; i < ballArray.length; i++) {
-                ballArray[i].update();
+                ballArray[i].update()
             }
         }
-
-        init();
-        animate();
-    });
-
-
-
+        init()
+        animate()
+    })
 
     return (
-        <section className="text-white text-shadow-xl body-font  justify-center ">
+        <section className="text-white mt-32 text-shadow-xl flex-col text-xl justify-center ">
+            <div className=" container mx-auto flex flex-col  items-start text-start md:pr-16 md:w-3/4 xl:w-full  lg:flex-grow ">
+                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium" >GRAVITY SIMULATION:</h1>
+                <p className="mb-8 leading-relaxed text-2xl  ">This is a simple model of gravity simulation where we can better understand how gravity and friction effects circular objects </p>
+            </div>
             <div className="container  mx-auto flex px-5 py-5 flex-row itmes-start">
+
                 <div>
-                    <button onClick={() => grRef.current.callFunction(1)} className=" mb-4 h-7 items-center  bg-blue-500 border-0  px-2  hover:bg-blue-600 rounded">start</button>
-                    <p>gravity</p>
-                    <div className="flex flex-row mb-4 ">
+
+                    <p>GRAVITY</p>
+                    <div className="flex flex-row mb-4  ">
 
                         <button onClick={decrementGravity} className=" h-7 w-7 mr-1 bg-blue-500 hover:bg-blue-600 rounded">-</button>
                         <span> {gravity} </span>
                         <button onClick={incrementGravity} className=" h-7 w-7 ml-1 bg-blue-500 hover:bg-blue-600 rounded">+</button>
 
                     </div>
+                    <p>FRICTION</p>
+                    <div className="flex flex-row mb-4 mt-1 ">
 
-                    <p>radius</p>
-                    <div className="flex flex-row min-w-min mb-4 ">
+                        <button onClick={decrementFriction} className=" h-7 w-7 mr-1 bg-blue-500 hover:bg-blue-600 rounded">-</button>
+                        <span> {friction.toFixed(2)} </span>
+                        <button onClick={incrementFriction} className=" h-7 w-7 ml-1 bg-blue-500 hover:bg-blue-600 rounded">+</button>
 
-                        <button onClick={decrementRadius} className=" h-7 w-7 mr-1 bg-blue-500 hover:bg-blue-600 rounded">-</button>
-                        <span> {radius} </span>
-                        <button onClick={incrementRadius} className=" h-7 w-7 ml-1 bg-blue-500 hover:bg-blue-600 rounded">+</button>
+                    </div>
+                    <p>BALLS</p>
+                    <div className="flex flex-row  mb-4 mt-1">
+
+                        <button onClick={decrementballs} className=" h-7 w-7 mr-1 bg-blue-500 hover:bg-blue-600 rounded">-</button>
+                        <span> {balls} </span>
+                        <button onClick={incrementBalls} className=" h-7 w-7 ml-1 bg-blue-500 hover:bg-blue-600 rounded">+</button>
 
                     </div>
                 </div>
 
-
-                {/* <Canvas ref={grRef} className="w-11/12 h-11/12 justify-center bg-white" /> */}
-                {/* <canvas className="w-11/12 h-11/12 justify-center "></canvas> */}
                 <canvas className="w-4/5 h-11/12 mx-auto justify-center bg-white bg-opacity-40 backdrop-blur-md rounded-2xl drop-shadow-lg"></canvas>
 
-
-
-
-
             </div>
-
-
         </section >
-
     )
-
 }
 export default BgOne
--- pages/room1.tsx
+++ pages/room1.tsx
@@ -8,19 +8,18 @@ import Image from 'next/image'
 const Room1: NextPage = () => {
   return (
     <section>
-      {/* <div className=" h-full w-full  fixed ">
-        <Image src="/pexels-satoshi-hirayama-22.jpg" layout="fill"
+      <div className="h-full w-full bg-gradient-to-tl from-violet-900 via-emerald-900 to-cyan-900 fixed ">
+        <Image src="/pexels-satoshi-hirayama-2.jpg" layout="fill"
           objectFit="cover" objectPosition="top" blurDataURL="data:..." priority
-          placeholder="blur absolute" />
+        />
 
 
-      </div> */}
-      <div className=" min-h-screen h-full w-full bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
+      </div>
+      <div className="   absolute min-h-screen h-full w-full ">
         <Head>
           <title>Room 1</title>
           <link rel="icon" href="/favicon/favicon.ico" />
         </Head>
-
         <Header />
         <BgOne />
 
